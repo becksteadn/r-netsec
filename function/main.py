ca2763ef4cf12ef7171c9fd22305602b96ca837f
Nathaniel Beckstead <becksteadn@gmail.com>
2019-08-18 21:53:41 -0400

    function works on cli

--- function/main.py
+++ function/main.py
@@ -1,4 +1,18 @@
+import requests
+import re
+
 def handler(event, context):
+    r = requests.get('https://api.pushshift.io/reddit/search/submission/?subreddit=netsec&limit=100')
+    for post in r.json()['data']:
+        if post['domain'] == "github.com":
+            m = re.search('github.com\/([a-zA-Z0-9-_]+)\/([a-zA-Z0-9_-]+)', post['url'])
+            try:
+                print('{}/{}'.format(m.group(1), m.group(2)))
+                s = requests.put('https://api.github.com/user/starred/{}/{}'.format(m.group(1), m.group(2)), headers={'Accept': 'application/vnd.github.v3+json'}, auth=requests.auth.HTTPBasicAuth('r-netsec', ''))
+                if s.status_code != 204:
+                    print(s.status_code)
+            except Exception as e:
+                pass
     return {}
 
 if __name__ == "__main__":
